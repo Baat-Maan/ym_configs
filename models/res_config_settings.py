b73dc9f1724694533fddd7a590213a74cb1f3a54
Ajay Mahato <ajjayymahato@gmail.com>
2022-08-18 23:05:36 +0530

    fix

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields
 
+
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
@@ -11,8 +12,10 @@ class ResConfigSettings(models.TransientModel):
     save_customer = fields.Boolean(string='Save Customer To Beta', config_parameter='ym_configs.save_customer')
     customer_endpoint = fields.Char(string='Customer Beta Endpoint', config_parameter='ym_configs.customer_endpoint')
 
-    save_customer_branch = fields.Boolean(string='Save Customer Branch To Beta', config_parameter='ym_configs.save_customer_branch')
-    customer_branch_endpoint = fields.Char(string='Customer Beta Endpoint', config_parameter='ym_configs.customer_branch_endpoint')
+    save_customer_branch = fields.Boolean(string='Save Customer Branch To Beta',
+                                          config_parameter='ym_configs.save_customer_branch')
+    customer_branch_endpoint = fields.Char(string='Customer Beta Endpoint',
+                                           config_parameter='ym_configs.customer_branch_endpoint')
 
     save_quotation = fields.Boolean(string='Save Quotation To Beta', config_parameter='ym_configs.save_quotation')
     quotation_endpoint = fields.Char(string='Quotation Beta Endpoint', config_parameter='ym_configs.quotation_endpoint')
@@ -23,8 +26,10 @@ class ResConfigSettings(models.TransientModel):
     rapid_sms_url = fields.Char(string='RapidSMS Api Endpoint', config_parameter='ym_configs.rapid_sms_url')
     rapid_sms_sender = fields.Char(string='RapidSMS Sender', config_parameter='ym_configs.rapid_sms_sender')
 
-    sales_head_contact = fields.Char(string='Sales Team Head', size=10, config_parameter='ym_configs.sales_head_contact')
-    accounts_head_contact = fields.Char(string='Accounts Team Head', size=10, config_parameter='ym_configs.accounts_head_contact')
+    sales_head_contact = fields.Char(string='Sales Team Head', size=10,
+                                     config_parameter='ym_configs.sales_head_contact')
+    accounts_head_contact = fields.Char(string='Accounts Team Head', size=10,
+                                        config_parameter='ym_configs.accounts_head_contact')
     ops_head_contact = fields.Char(string='Ops Team Head', size=10, config_parameter='ym_configs.ops_head_contact')
 
     sap_api_url = fields.Char(string='SAP API Endpoint', config_parameter='ym_configs.sap_api_url')
